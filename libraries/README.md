58255fad68163567add16cd4073fc43fa7bbc303
chromium7 <christofersaputra29@gmail.com>
2022-01-29 20:26:01 +0700

    Add spotify get auth URL

--- libraries/README.md
+++ libraries/README.md
@@ -3,9 +3,19 @@
 1.  Go to this page https://developer.spotify.com/dashboard
 2.  Create a new application in spotify
 3.  Store Client ID and Client Secret in config_local.py
+4.  Add `redirect uri`
 
-Since we are not using a web server, we will go with the `client credentials` option of `OAuth 2.0` which means that only endpoints that do not access user information can be accessed.
+Since we will be using the authorization code flow of `OAuth 2.0`,
+which require user to login to the app we just created.
+
+To login your spotify account to the app, open up `python` 
+and put in the following code to get the login URL:
+
+```py
+from libraries.spotify import get_authorization_url
+print(get_authorization_url())
+```
 
 More on this:
 
-> https://developer.spotify.com/documentation/general/guides/authorization/client-credentials/
+> https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
--- libraries/spotify.py
+++ libraries/spotify.py
@@ -1,9 +1,50 @@
+import json
 import logging
 import requests
-from typing import Any, Dict
 from base64 import b64encode
+from datetime import datetime, timedelta
+from typing import Any, Dict
+from urllib.parse import urlencode
 
 from config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+
+
+BASE_URL = 'https://api.spotify.com/v1/'
+
+
+def get_authorization_url() -> str:
+    """
+    Get URL for user to give access of their spotify account
+    to our application.
+    """
+    # More on scope: https://developer.spotify.com/documentation/general/guides/authorization/scopes/
+    scope = [
+        'user-read-playback-state',
+        'user-modify-playback-state',
+        'user-read-currently-playing',
+        'user-read-recently-played',
+    ]
+    url = 'https://accounts.spotify.com/authorize?'
+    parameter = {
+        'client_id': SPOTIFY_CLIENT_ID,
+        'scope': ' '.join(scope),
+        'response_type': 'code',
+        # TODO: create this API endpoint
+        'redirect_uri': 'http://127.0.0.1:8001/api/spotify/redirect',
+    }
+    return url + urlencode(parameter)
+
+
+def get_headers() -> Dict[str, Any]:
+    """
+    Get the headers required to make a request to Spotify's API
+    """
+    token = get_access_token()
+    headers = {
+        'Authorization': f'{token["token_type"]} {token["access_token"]}',
+        'Content-Type': 'application/json'
+    }
+    return headers
 
 
 def get_access_token() -> Dict[str, Any]:
@@ -11,6 +52,17 @@ def get_access_token() -> Dict[str, Any]:
     Get an access token from Spotify
     Send a POST request with user credentials to Spotify's OAuth endpoint
     """
+    now = datetime.now()
+    try:
+        with open('spotify.json', 'r') as f:
+            access_token: Dict = json.load(f)
+            expires_at = datetime.fromtimestamp(access_token.get('expires_at', 0))
+            # Check if access token is still valid
+            if expires_at > now:
+                return access_token
+    except (json.JSONDecodeError, FileNotFoundError):
+        pass
+
     url = 'https://accounts.spotify.com/api/token'
     headers = {
         'Authorization': 'Basic {}'.format(
@@ -25,7 +77,12 @@ def get_access_token() -> Dict[str, Any]:
         # 10 seconds timeout
         response = requests.post(url, headers=headers, data=data, timeout=10)
         response.raise_for_status()
-        return response.json()
+        data = response.json()
+        data['expires_at'] = (now + timedelta(seconds=data['expires_in'])).timestamp()
+        # Write access token to file
+        with open('spotify.json', 'w') as f:
+            json.dump(data, f)
+        return data
     except requests.RequestException as e:
         logging.error(f'Error while getting access token: {e}')
         return {}
